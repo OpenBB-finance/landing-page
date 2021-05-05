0f7dbe01b7b86307e1b2bbdf6e8d2c4b51d50a84
jose-donato <zmcdonato@gmail.com>
2021-05-05 15:32:06 +0100

    initial files

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,46 @@
+import Header from '@/components/Header'
+import { motion } from 'framer-motion'
 import Head from 'next/head'
-
+import Image from "next/image"
+const title = `Gamestonk Terminal is a
+DIY, Meme Stock Version of
+Bloomberg Terminal`
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <main className="flex flex-col items-center justify-center">
+        <section className="relative w-full h-screen">
+          <Image className="h-96 z-0" src="/hero.png" layout="fill" objectFit="cover" objectPosition="center" />
+          <div className="absolute z-10 p-20">
+            <motion.h1 initial={{x: -200}} animate={{x: 0}} className="font-semibold text-4xl tracking-wide w-96">{title}</motion.h1>
+            <h4 className="italic text-xl mt-4">- VICE</h4>
+            <p style={{maxWidth: 600}} className="mt-4">Gamestonk Terminal provides a modern Python-based integrated environment for investment research, that allows the average-joe retail trader to leverage state-of-the-art Data Science and Machine Learning technologies.</p>
+            <p className="mt-4 tracking-tight italic font-semibold">#weliketheterminal</p>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <div className="mt-16 flex gap-x-8">
+              <motion.button 
+              whileHover={{scale: 1.05}}
+              whileTap={{scale: 1}}
+              className="focus:outline-none bg-black text-primary w-48 h-16 font-semibold text-lg rounded-full"
+              >
+                Download Terminal
+              </motion.button>
+              <motion.button 
+              whileHover={{scale: 1.05}}
+              whileTap={{scale: 1}}
+              className="focus:outline-none bg-white text-primary w-48 h-16 font-semibold text-lg rounded-full"
+              >
+                Explore Features
+              </motion.button>
+            </div>
+          </div>
+        </section>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#0dc4a5"
+      }
+    },
   },
   variants: {
     extend: {},
